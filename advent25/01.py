f42affd8db7426d58badf2b963eb469d7dfef4ff
Hugo Findlay <hugo.findlay@gmail.com>
2025-12-02 20:13:51 +0000

    day 1

---
+++ advent25/01.py
@@ -0,0 +1,25 @@
+file = open("01.txt")
+data = file.readlines()
+
+dial, total = 50, 0
+
+for instruction in data:
+    direction = instruction[0]
+    gross_magnitude = int(instruction[1:])
+    new_dial = dial
+
+    total += gross_magnitude // 100
+    fine_magnitude = gross_magnitude % 100
+
+    if direction == 'L':
+        new_dial = dial + fine_magnitude
+    else:
+        new_dial = dial - fine_magnitude
+
+    if (dial != 0):
+        if (new_dial <= 0 or new_dial >= 100):
+            total += 1
+
+    dial = new_dial % 100
+
+print(total)
