b8c5c6700ef6a303a194a6e818276d29b76dd98b
Hugo Findlay <hugo.findlay@gmail.com>
2025-12-04 21:41:58 +0000

    day 4 refactor

--- advent25/04.py
+++ advent25/04.py
@@ -1,52 +1,33 @@
-grid = []
-for line in open("04.txt"):
-    row = []
-    for space in line.strip():
-        row.append(space)
-    grid.append(row)
+grid = {}
+for y, line in enumerate(open("04.txt")):
+    for x, space in enumerate(line.strip()):
+        if space == '@':
+            grid.update({(y,x): space})
 
-def print_rolls(grid):
-    for row in grid:
-        print(''.join(row))
+def check(grid):
+    new_grid, total = {}, 0
+    keys = grid.keys()
 
-def check(x, y):
-    adjacent_rolls = 0
+    for spot in keys:
+        y, x = spot
+        adjacent_rolls = 0
 
-    for i in [-1, 0, 1]:
-        for j in [-1, 0, 1]:
-            t_y, t_x = y + j, x + j
-            if (t_x >= 0 and t_x < len(grid[x]) and t_y >= 0 and t_y < (len(grid))):
-                if grid[t_y][t_x] == '@' or grid[t_y][t_x] == 'x':
+        for i in [-1,0,1]:
+            for j in [-1,0,1]:
+                if (y-i, x-j) in keys:
                     adjacent_rolls += 1
 
-    if adjacent_rolls <= 4:
-        grid[y][x] = 'x'
-        return 1
-    else:
-        return 0
+        if adjacent_rolls <= 4:
+            total += 1
+        else:
+            new_grid.update({spot: '@'})
 
-def remove_rolls(grid):
-    rolls = 0
-    for y, row in enumerate(grid):
-        for x, spot in enumerate(row):
-            if spot == '@':
-                rolls += check(x,y)
-    return grid, rolls
+    return new_grid, total
 
-def replace_empty(grid):
-    for y, row in enumerate(grid):
-        for x, spot in enumerate(row):
-            if spot == 'x':
-                grid[y][x] = '.'
-    return grid
-
-total_rolls = 0
-retrived_rolls = -1
+total_rolls, retrived_rolls = 0, -1
 
 while retrived_rolls != 0:
-    newGrid, retrived_rolls = remove_rolls(grid)
-    grid = replace_empty(newGrid)
+    grid, retrived_rolls = check(grid)
     total_rolls += retrived_rolls
 
-print_rolls(grid)
 print(total_rolls)
