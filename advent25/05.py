ba3ff87c754dc970731bdb580a7d3b1944f67859
Hugo Findlay <hugo.findlay@gmail.com>
2025-12-05 14:11:22 +0000

    day 5

---
+++ advent25/05.py
@@ -0,0 +1,43 @@
+x = open("05.txt").read().split('\n\n')
+
+def collapse(listOf):
+    new_ranges = []
+    for start, stop in listOf:
+        added = False
+
+        for i, (lower, upper) in enumerate(new_ranges):
+            if start <= upper and start > lower and stop > upper:       # lower--start/upper--stop
+                new_ranges[i] = (lower, stop)
+                added = True
+                break
+            elif start < lower and stop <= upper and stop >= lower:     # start--lower/stop--upper
+                new_ranges[i] = (start, upper)
+                added = True
+                break
+            elif start <= lower and stop >= upper:                      # start/lower---upper/stop
+                new_ranges[i] = (start, stop)
+                added = True
+                break
+            elif start >= lower and stop <= upper:                      # lower-start---stop-upper
+                added = True
+                break
+
+        if (not added):                                                 # lower-upper---start-stop
+            new_ranges.append((start, stop))
+    return new_ranges
+
+fresh_ranges = []
+for range in x[0].split('\n'):
+    ids = range.split('-')
+    fresh_ranges.append((int(ids[0]), int(ids[1])))
+
+old_ranges = []
+while len(old_ranges) != len(fresh_ranges):
+    old_ranges = fresh_ranges
+    fresh_ranges = collapse(fresh_ranges)
+
+total = 0
+for lower_boundary, upper_boundary in old_ranges:
+    total += (upper_boundary - lower_boundary) + 1
+
+print(total)
